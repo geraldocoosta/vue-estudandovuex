bc32d25aa2cdca0cd5726e3f8c369722f855e8e6
Geraldo Costa <geraldocoosta@gmail.com>
2019-10-22 20:00:44 -0300

    criando getter para fazer um calculo centralizado

--- src/store/store.js
+++ src/store/store.js
@@ -6,5 +6,12 @@ vue.use(vuex);
 export default new vuex.Store({
   state: {
     produtos: []
+  },
+  getters: {
+    valorTotal(state) {
+      return state.produtos
+        .map(p => p.quantidade * p.preco)
+        .reduce((total, atual) => total + atual, 0);
+    }
   }
 });
